7634053f4eb515803ddb7e9658817d5556586211
iquery-001 <kota@vaga.tk>
2020-03-10 14:52:08 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+class School
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        # binding.pry
+        @roster.each do |(key, value)|
+            @roster[key] = value.sort
+        end
+    end
+end
